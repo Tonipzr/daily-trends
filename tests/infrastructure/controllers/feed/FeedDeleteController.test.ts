c54707da97e9563ca1fe5de0e3d57680494f6a74
Antonio Pozo Ruiz <porutoni@gmail.com>
2025-02-23 19:12:30 +0100

    chore(tests): add api controllers tests

---
+++ tests/infrastructure/controllers/feed/FeedDeleteController.test.ts
@@ -0,0 +1,40 @@
+import { Service } from '../../../../src/application/shared/Service.ts'
+import { FeedDeleteController } from '../../../../src/infrastructure/api/controllers/feed/FeedDeleteController.ts'
+
+describe('FeedDeleteController', () => {
+  let controller: FeedDeleteController
+  let mockService: jest.Mocked<Service<boolean>>
+
+  beforeEach(() => {
+    mockService = {
+      execute: jest.fn()
+    }
+
+    controller = new FeedDeleteController(mockService)
+  })
+
+  it('should delete the feed when valid params', async () => {
+    mockService.execute.mockResolvedValue(true)
+    const result = await controller.run({ id: '1' }, {})
+
+    expect(result).toEqual(true)
+    expect(mockService.execute).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return error message when invalid params', async () => {
+    const result = await controller.run({}, {})
+
+    expect(result).toEqual('Invalid params')
+    expect(mockService.execute).not.toHaveBeenCalled()
+  })
+
+  it('should return error message', async () => {
+    const errorMessage = 'Error message'
+
+    mockService.execute.mockRejectedValue(new Error(errorMessage))
+    const result = await controller.run({ id: '1' }, {})
+
+    expect(result).toEqual(errorMessage)
+    expect(mockService.execute).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ tests/infrastructure/controllers/feed/FeedSaveController.test.ts
@@ -0,0 +1,30 @@
+import { Service } from '../../../../src/application/shared/Service.ts'
+import { FeedSaveController } from '../../../../src/infrastructure/api/controllers/feed/FeedSaveController.ts'
+
+describe('FeedSaveController', () => {
+  let controller: FeedSaveController
+  let mockService: jest.Mocked<Service<void>>
+
+  beforeEach(() => {
+    mockService = {
+      execute: jest.fn()
+    }
+
+    controller = new FeedSaveController(mockService)
+  })
+
+  it('should send the params to the service', async () => {
+    const feedBody = {
+      title: 'string',
+      url: 'string',
+      author: 'string',
+      category: 'string',
+      publishedAt: new Date().toISOString()
+    }
+
+    await controller.run(undefined, feedBody)
+
+    expect(mockService.execute).toHaveBeenCalledWith(feedBody)
+    expect(mockService.execute).toHaveBeenCalledTimes(1)
+  })
+})
